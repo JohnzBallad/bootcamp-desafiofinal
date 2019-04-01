50d0e94b7ad7c4cf70d34feb6d59adbab144b543
Jonathan Galdino <johnzballad@gmail.com>
2019-04-01 15:58:22 -0300

    Middleware de Autenticação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,20 @@ const express = require('express')
 
 const routes = express.Router()
 
+/* Middlewares */
+const authMiddleware = require('./app/middlewares/auth')
+
 /* Controllers */
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+const PreferenceController = require('./app/controllers/PreferenceController')
 
 routes.post('/signup', UserController.store)
+
+routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware)
+
+routes.post('/preferences', PreferenceController.store)
 
 module.exports = routes
