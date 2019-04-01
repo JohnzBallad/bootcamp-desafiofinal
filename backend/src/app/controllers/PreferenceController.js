657ad45ad8a82b6ac772ce757b6cdca417aa9f24
Jonathan Galdino <johnzballad@gmail.com>
2019-04-01 17:11:34 -0300

    Verificações
    Essa verificação tem como função validar se o usuário já definiu suas preferências na primeira vez que fez o login. Se sim, então não permite alterar as preferências por essa rota.
    Caso contrário, cria as preferências normalmente.
    
    Agora está alterando a váriavel que controla se o usuário está entrando pela primeira vez na aplicação. Eu tinha esquecido de fazer isso antes.

--- backend/src/app/controllers/PreferenceController.js
+++ backend/src/app/controllers/PreferenceController.js
@@ -7,9 +7,14 @@ class PreferenceController {
     const user = await User.findOne({ where: { id: req.userId } })
 
     if (!user.first_time) {
-      console.log(user.first_time)
       return res.status(401).json({
         error: 'You are not allowed to change prefences from this route.'
+      })
+    }
+
+    if (await Preference.findOne({ where: { user_id: req.userId } })) {
+      return res.status(401).json({
+        error: 'Preferences already exists. Alter it from profile page. '
       })
     }
 
@@ -22,6 +27,9 @@ class PreferenceController {
       marketing,
       gestao
     })
+
+    user.first_time = false
+    await user.save()
 
     return res.json(preference)
   }
