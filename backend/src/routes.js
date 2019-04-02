05b12f2cec93c78b5fab077c0f1adf21de0e7a3e
Jonathan Galdino <johnzballad@gmail.com>
2019-04-01 23:56:22 -0300

    Rotas de subscribers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,6 +10,7 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const PreferenceController = require('./app/controllers/PreferenceController')
 const MeetupController = require('./app/controllers/MeetupController')
+const SubscribeController = require('./app/controllers/SubscribeController')
 
 routes.post('/signup', UserController.store)
 routes.post('/sessions', SessionController.store)
@@ -20,5 +21,8 @@ routes.post('/preferences', PreferenceController.store)
 routes.put('/profile', UserController.update)
 
 routes.post('/meetups', MeetupController.store)
+routes.get('/meetups', MeetupController.index)
+
+routes.post('/meetups/:meetupId/subscribe', SubscribeController.store)
 
 module.exports = routes
