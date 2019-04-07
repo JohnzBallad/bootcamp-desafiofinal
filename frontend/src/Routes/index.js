d97572df2a91728b79590524f83e51aeeddfdf59
Jonathan Galdino <johnzballad@gmail.com>
2019-04-07 16:05:55 -0300

    Rota da página de criar conta

--- frontend/src/Routes/index.js
+++ frontend/src/Routes/index.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Signin from '../pages/Signin';
+import Signup from '../pages/Signup';
 
 const Routes = () => (
   <BrowserRouter>
     <Fragment>
       <Switch>
         <Route exact path="/" component={Signin} />
+        <Route exact path="/signup" component={Signup} />
       </Switch>
     </Fragment>
   </BrowserRouter>
