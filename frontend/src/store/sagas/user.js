1845aad3430e6e4580563f8833ad72c0cb47ab4e
Jonathan Galdino <johnzballad@gmail.com>
2019-04-08 00:17:42 -0300

    Organização de comentários, mudança de rotas

--- frontend/src/store/sagas/user.js
+++ frontend/src/store/sagas/user.js
@@ -1,5 +1,6 @@
 import { call, put } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
+import { push } from 'connected-react-router';
 
 import api from '../../services/api';
 import { Creators as UserActions } from '../ducks/user';
@@ -9,8 +10,11 @@ export function* userLogin(action) {
     const { data } = yield call(api.post, '/sessions', action.payload.credentials);
 
     yield put(UserActions.userLoginSuccess(data));
+
     // Salva o token no localStorage do browser.
     localStorage.setItem('@meetapp.usertoken', data.token);
+
+    // Salva os dados do usuário no localStorage do browser.
     localStorage.setItem(
       '@meetapp.userinfo',
       JSON.stringify({
@@ -24,6 +28,10 @@ export function* userLogin(action) {
     toast.success('Login Successful', {
       position: toast.POSITION.TOP_CENTER,
     });
+
+    if (data.user.first_time) {
+      yield put(push('/welcome'));
+    }
   } catch (err) {
     yield put(UserActions.userLoginFailure(err.response.data.error));
     toast.error(err.response.data.error, {
@@ -37,9 +45,12 @@ export function* userCreate(action) {
     const { data } = yield call(api.post, '/signup', action.payload.data);
 
     yield put(UserActions.userLoginSuccess(data));
+
     toast.success('Account created', {
       position: toast.POSITION.TOP_CENTER,
     });
+
+    yield put(push('/'));
   } catch (err) {
     yield put(UserActions.userLoginFailure(err.response.data.error));
     toast.error(err.response.data.error, {
