9771c7ce8eb2cf304559fd57345fefeb6394bbd0
Jonathan Galdino <johnzballad@gmail.com>
2019-04-07 21:04:59 -0300

    Salvando alguns dados no localStorage
    Salvando alguns dados do usuário no localStorage

--- frontend/src/store/sagas/user.js
+++ frontend/src/store/sagas/user.js
@@ -1,7 +1,7 @@
 import { call, put } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
-import api from '../../services/api';
 
+import api from '../../services/api';
 import { Creators as UserActions } from '../ducks/user';
 
 export function* userLogin(action) {
@@ -9,6 +9,18 @@ export function* userLogin(action) {
     const { data } = yield call(api.post, '/sessions', action.payload.credentials);
 
     yield put(UserActions.userLoginSuccess(data));
+    // Salva o token no localStorage do browser.
+    localStorage.setItem('@meetapp.usertoken', data.token);
+    localStorage.setItem(
+      '@meetapp.userinfo',
+      JSON.stringify({
+        id: data.user.id,
+        email: data.user.email,
+        first_time: data.user.first_time,
+        preference_id: data.user.preference_id,
+      }),
+    );
+
     toast.success('Login Successful', {
       position: toast.POSITION.TOP_CENTER,
     });
