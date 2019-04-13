14b61f43cf963fbae1d0931b28e2c9ecb94ad30a
Jonathan Galdino <johnzballad@gmail.com>
2019-04-13 17:53:31 -0300

    Redux da aba de pesquisa

--- frontend/src/store/ducks/index.js
+++ frontend/src/store/ducks/index.js
@@ -4,10 +4,12 @@ import { connectRouter } from 'connected-react-router';
 import user from './user';
 import preference from './preference';
 import meetup from './meetup';
+import search from './search';
 
 export default history => combineReducers({
   router: connectRouter(history),
   user,
   preference,
   meetup,
+  search,
 });
