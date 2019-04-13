a83137f00235d53b7b1c5106c830ff290c3f2c23
Jonathan Galdino <johnzballad@gmail.com>
2019-04-13 17:53:48 -0300

    Conectando o header as rotas e ao redux de pesquisa

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,27 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import PropTypes from 'prop-types';
 import PersonIcon from '@material-ui/icons/PersonOutlineOutlined';
+
+import { Creators as SearchActions } from '../../store/ducks/search';
 
 import {
   Logo, Container, Button, LeftWrapper, Person,
 } from './styles';
 import WhiteLogo from '../../assets/logo-white.svg';
 
-const Header = () => (
-  <Container>
-    <LeftWrapper>
-      <Logo src={WhiteLogo} alt="logo white" />
+class Header extends Component {
+  static propTypes = {
+    showSearchInput: PropTypes.func.isRequired,
+    hideSearchInput: PropTypes.func.isRequired,
+    visible: PropTypes.bool.isRequired,
+    history: PropTypes.shape({
+      push: PropTypes.func,
+    }).isRequired,
+    location: PropTypes.shape({
+      pathname: PropTypes.string,
+    }).isRequired,
+  };
 
-      <Button type="button">Início</Button>
+  handleGoInit = () => {
+    const { history, location } = this.props;
 
-      <Button type="button">Buscar</Button>
+    if (location.pathname !== '/dashboard') {
+      history.push('/dashboard');
+    }
+  };
 
-      <Button type="button">Novo meetup</Button>
-    </LeftWrapper>
+  handleSearch = () => {
+    const { showSearchInput, hideSearchInput, visible } = this.props;
 
-    <Person>
-      <PersonIcon />
-    </Person>
-  </Container>
-);
+    if (visible) {
+      hideSearchInput();
+    } else {
+      showSearchInput();
+    }
+  };
 
-export default Header;
+  handleNewMeetup = (e) => {
+    // const { history } = this.props;
+    // history.push('/dashboard/meetup/create');
+  };
+
+  render() {
+    return (
+      <Container>
+        <LeftWrapper>
+          <Logo src={WhiteLogo} alt="logo white" />
+
+          <Button onClick={this.handleGoInit} type="button">
+            Início
+          </Button>
+
+          <Button onClick={this.handleSearch} type="button">
+            Buscar
+          </Button>
+
+          <Button onClick={this.handleNewMeetup} type="button">
+            Novo meetup
+          </Button>
+        </LeftWrapper>
+
+        <Person>
+          <PersonIcon />
+        </Person>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  visible: state.search.visible,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(SearchActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(Header));
